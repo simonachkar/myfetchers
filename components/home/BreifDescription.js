0590dec6b85bd1bf9187ba386ab65fb924bbb621
Simon Achkar <alachkarsimon@gmail.com>
2018-09-25 10:37:17 -0300

    Installed Material UI and used grid system

--- components/home/BreifDescription.js
+++ components/home/BreifDescription.js
@@ -1,32 +1,62 @@
-const BreifDescription = () => (
-  <section>
+import Grid from '@material-ui/core/Grid';
 
+const BreifDescription = () => (
+  <section style={{ flexGrow: 1 }}>
     <h1>How it works ?</h1>
-    <div className="wrapper">
-      <div className="left-column">
-        <h2>Tasker</h2>
-        <ul>
-          <li>Post a Task</li>
-          <li>Wait for fetchers to place their bids</li>
-          <li>Accept the best bid</li>
-          <li>Get things done!</li>
-        </ul>
-      </div>
-      <div className="right-column">
-        <h2>Fetcher</h2>
-        <ul>
-          <li>Find tasks to Fetch</li>
-          <li>Place your bid</li>
-          <li>Get accepted</li>
-          <li>Get Paid!</li>
-        </ul>
-      </div>
-    </div>
+    <Grid
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+      spacing={40}
+    >
+      <Grid item sm={6} xs={12}>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={0}
+        >
+          <Grid item xs={3}>
+            <h2>Tasker</h2>
+          </Grid>
+          <Grid item xs={9}>
+            <ul>
+              <li>Post a Task</li>
+              <li>Wait for fetchers to place their bids</li>
+              <li>Accept the best bid</li>
+              <li>Get things done!</li>
+            </ul>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item sm={6} xs={12}>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={0}
+        >
+          <Grid item xs={3}>
+            <h2>Fetcher</h2>
+          </Grid>
+          <Grid item xs={9}>
+            <ul>
+              <li>Find tasks to Fetch</li>
+              <li>Place your bid</li>
+              <li>Get accepted</li>
+              <li>Get Paid!</li>
+            </ul>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
     <style jsx>
       {`
         section {
-          padding: 50px 70px;
-          position: relative;
+          padding: 50px 100px;
         }
         h1 {
           text-align: center;
@@ -35,31 +65,12 @@ const BreifDescription = () => (
         h2 {
           margin-bottom: 10px;
         }
-        .wrapper {
-          padding: 10px;
-          display: flex;
-          align-self: center;
-          justify-content: space-around;
-        }
-        .left-column {
-          display: flex;
-          align-items: center;
-          float: left;
-        }
-        .right-column {
-          display: flex;
-          align-items: center;
-          right: 10%;
-        }
         ul {
           list-style: none;
         }
         ul li:before {
           content: '✓';
           margin-right: 10px;
-        }
-        li {
-          padding-left: 30px;
         }
       `}
     </style>
