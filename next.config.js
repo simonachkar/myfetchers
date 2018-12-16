ba14937dbed53e433b1ed204adc1cb37daaa208d
Simon Achkar <alachkarsimon@gmail.com>
2018-12-16 03:53:49 +0200

    Changed nextjs configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/how-it-works': { page: '/how-it-works' },
+      '/about': { page: '/about' },
+      '/privacy-policy': { page: '/privacy-policy' },
+    };
+  },
+};
